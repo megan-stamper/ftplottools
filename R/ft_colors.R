ff30a37cff16d0b0d7a501ac42614429f9b4c23c
Oliver Elliott <oliver.elliott@ft.com>
2016-10-19 12:59:18 +0100

    correct description file and add ft_colors function

---
+++ R/ft_colors.R
@@ -0,0 +1,19 @@
+
+
+ft_colors <- function(palette = "main"){
+  main <- c("#9e2f50","#ff7f8a","#3a70af","#f6801a","#2bbbbf","#410057","#505050","#99c6fb","#27757b","#8b572a")
+  greys <- c("#b0b0b0","#999999","#777777","#505050","#333333","#000000")
+  reds <- c("#ebbcb3","#df9c92","#d17c70","#c36256","#b1493f","#9e2f50")
+  blues <- c("#bcd7e5","#a9cadc","#8ab5cd","#75a5c2","#598caf","2e6e9e")
+  paired <- c("#9e2f50","#ff7f8a","#3a70af","#99c6fb","#c85f04","#f29d53","#27757b","2bbbbf","#410057","#f3dee3")
+  pal_out <- switch(palette,
+                    main = main,
+                    greys = greys,
+                    redds = reds,
+                    blues = blues,
+                    paired = paired)
+  if(length(pal_out) == 0){
+    stop("Palette not found")
+  }
+  return(pal_out)
+}
